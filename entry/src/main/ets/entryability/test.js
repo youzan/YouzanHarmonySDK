9a029b6cfa733f5bb6e0dd337dbe385bf39edaf4
liusaideng <43210801+pepperer@users.noreply.github.com>
2024-06-20 20:40:50 +0800

    开店SDK 鸿蒙 Sample

---
+++ entry/src/main/ets/entryability/test.js
@@ -0,0 +1,5 @@
+
+const content =  "Mozilla/5.0 (Phone; OpenHarmony 4.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36  ArkWeb/4.1.6.1 Mobile Android kdtUnion_clientKeyMozilla/5.0 (Linux; Android 12; M2104K10AC Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.72 MQQBrowser/6.2 TBS/046281 Mobile Safari/537.36 youzan_container_android/2.10.0 Dalvik/2.1.0 (Linux; Android 12; M2104K10AC Build/SP1A.210812.016; App/com.youzanyun.sdk.sample.x5_v1.0) kdtUnion_0073bccbaf5369028a"
+
+console.log(/android/gi.test(content));
+console.log(JSON.stringify("ssss"));
